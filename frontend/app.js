abf3948f137b96e59a6664ef07cef9ad795459f0
Mishra <Mishra.IshuKumar@bcg.com>
2025-12-10 00:18:53 +0530

    Minor changes

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,5 @@
-const API_BASE = 'http://localhost:8000';
-// const API_BASE = 'https://bcg-pitchpilot-backend.onrender.com';
+// const API_BASE = 'http://localhost:8000';
+const API_BASE = 'https://bcg-pitchpilot-backend.onrender.com';
 
 // const api = {
 //   async catalog() {
